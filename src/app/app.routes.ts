3fb3a0e22cea02d99a4fbce71f78ae61c3a02087
hadil kharroubi <hadilkharroubi85@gmail.com>
2025-10-18 20:59:55 +0100

    second

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Logincomponent } from './logincomponent/logincomponent';
 import { Maincomponent } from './maincomponent/maincomponent';
 import { Homecomponenet } from './homecomponenet/homecomponenet';
 import { Library } from './library/library';
+import { Signin } from './signin/signin';
 
 export const routes: Routes = [{path:'login',component:Logincomponent},{path:'search',component:Maincomponent},{path:'home',component:Homecomponenet}
-    ,{path:'library',component:Library}
+    ,{path:'library',component:Library},{path:'sign',component:Signin}
 ];
